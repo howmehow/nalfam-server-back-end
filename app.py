dc014f158282b2852e188a7537c82038f6feb171
lukerhys <hello@lukemelvin.com>
2020-04-27 17:08:23 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import requests
+import json
+import datetime
+import random
+response = requests.get('http://localhost:5000/devices/')
+devices = response.json()
+# packages = json.dumps(data, indent=2)
+for device in devices:
+    device['timeStamp'] = str(datetime.datetime.now()).split('.')[0]
+    device['upload'] = random.getrandbits(10)
+    device['download'] = random.getrandbits(20)
+jason = json.dumps(devices, indent=2)
+# print(devices)
+print(jason)
+# packages=json.loads(data)
+# time = datetime.now()
+# for package in packages:
+#     package["TimeStamp"] = time
+
+# print(packages)
